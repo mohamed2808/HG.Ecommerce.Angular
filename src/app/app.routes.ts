c3e0afd5545f8910950b35c48bd6083002fe0d92
Hagag <mohamedabohagag54@gmail.com>
2025-09-20 20:57:54 +0300

    update_routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,24 @@
-import path from 'path';
-import { App } from './app';
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login-component/login-component';
-import { Component } from '@angular/core';
-import { register } from 'module';
-import { RegisterComponent } from './features/auth/register/register-component/register-component';
-import { AuthComponent } from './features/auth/auth-component/auth-component';
-import { HomeComponent } from './features/catalog/home-component/home-component';
 import { ProductDetailsComponent } from './features/catalog/productDetails/product-details-component/product-details-component';
+import { HomeComponent } from './features/catalog/home-component/home-component';
 import { ProductListComponent } from './features/catalog/productList/product-list-component/product-list-component';
+import { LoginComponent } from './features/auth/login/login-component/login-component';
+import { RegisterComponent } from './features/auth/register/register-component/register-component';
 
 export const routes: Routes = [
-  { path: 'auth', children:[ {
-    path: 'login' , component: LoginComponent
-  },
-   {
-    path: 'register', component: RegisterComponent
-   }
-]
-},
-  {path:'',component: HomeComponent},
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductListComponent },
   {
-  path: 'products/:id',
-  component: ProductDetailsComponent
-},
-  {path:'products',component:ProductListComponent},
+    path: 'products/:id',
+    component: ProductDetailsComponent,
+    data: { renderMode: 'client' } // ← هذه السطر يحول الصفحة لـ CSR
+  },
+  {
+    path: 'auth',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  },
   { path: '**', redirectTo: '' }
 ];
-
